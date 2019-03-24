018489e080c91b3ddca2885faf12b55cd3d3df67
JeronimoMontero <jeronimo_montero@t-online.de>
2019-03-24 21:50:11 +0100

    added training

--- parser.py
+++ parser.py
@@ -13,37 +13,38 @@ if __name__ == '__main__':
     
     data_parsed = dict()
     for subject in sorted(os.listdir(path)):
+        if 'Fach' in subject:
+            if os.path.isfile(os.path.join(path, subject)):
+                with open(os.path.join(path, subject), 'r') as file:
+                    print(subject)
+                    data = file.read()
+                topic = subject.replace('.txt', '')
+                data_parsed[topic] = dict()
+                data_parsed[topic]['info'] = data.split('\n####\n')[0].strip()
 
-        if os.path.isfile(os.path.join(path, subject)):
-            with open(os.path.join(path, subject), 'r') as file:
-                print(subject)
-                data = file.read()
-            topic = subject.replace('.txt', '')
-            data_parsed[topic] = dict()
-            data_parsed[topic]['info'] = data.split('\n####\n')[0].strip()
-            index_list = re.findall('\d\d*\.\s*\t', data.split('\n####\n')[1])
-            content_list = list(filter(None, re.split('\d\d*\.\s*\t', data.split('\n####\n')[1])))
-            for index in index_list:
-                index = int(index.strip()[:-1])
-                raw = list(filter(None, re.split('(.*?[!?\.:]+)', content_list[index - 1], flags=re.DOTALL)))
+                index_list = re.findall('\d\d*\.\s*\t', data.split('\n####\n')[1])
+                content_list = list(filter(None, re.split('\d\d*\.\s*\t', data.split('\n####\n')[1])))
+                for index in index_list:
+                    index = int(index.strip()[:-1])
+                    #raw = list(filter(None, re.split('(.*?[!?\.:]+)', content_list[index - 1], flags=re.DOTALL)))
+                    raw = list(filter(None, re.split('\n', content_list[index - 1], flags=re.DOTALL)))
+                    data_parsed[topic][index] = {}
+                    data_parsed[topic][index]['question'] = raw[0].replace('\n', '').replace('\t', '').strip()
+                    if ' x ' in ''.join(raw[1:]):
+                        content = list(filter(None, re.split('[abc]\)', ''.join(raw[1:]), flags=re.DOTALL)))[1:]
+                        data_parsed[topic][index]['type'] = 'multiple_choice'
+                        data_parsed[topic][index]['answer'] = {}
+                        data_parsed[topic][index]['answer']['correct'] = []  # right answers
+                        data_parsed[topic][index]['answer']['choice'] = []  # possible answers
 
-                data_parsed[topic][index] = {}
-                data_parsed[topic][index]['question'] = raw[0].replace('\n', '').replace('\t', '').strip()
-                if ' x \n' in raw[1]:
-                    content = list(filter(None, re.split('[abc]\)', raw[1], flags=re.DOTALL)))[1:]
-                    data_parsed[topic][index]['type'] = 'multiple_choice'
-                    data_parsed[topic][index]['answer'] = {}
-                    data_parsed[topic][index]['answer']['correct'] = []  # right answers
-                    data_parsed[topic][index]['answer']['choice'] = []  # possible answers
+                        for number, answer in enumerate(content):
+                            if answer.strip()[-1] == 'x':
+                                answer = answer.replace(' x', '')
+                                data_parsed[topic][index]['answer']['correct'].append(number)
+                            data_parsed[topic][index]['answer']['choice'].append(answer.strip())
+                    else:
+                        data_parsed[topic][index]['type'] = 'text'
+                        data_parsed[topic][index]['answer'] = ''.join(raw[1:]).replace('\n', '').replace('\t', '').strip()
 
-                    for number, answer in enumerate(content):
-                        if answer.strip()[-1] == 'x':
-                            answer = answer.replace(' x', '')
-                            data_parsed[topic][index]['answer']['correct'].append(number)
-                        data_parsed[topic][index]['answer']['choice'].append(answer.strip())
-                else:
-                    data_parsed[topic][index]['type'] = 'text'
-                    data_parsed[topic][index]['answer'] = ''.join(raw[1]).replace('\n', '').replace('\t', '').strip()
-
-    with open(os.path.join(path, 'jagdschein.json'), 'w') as outfile:
-        json.dump(data_parsed, outfile)
+        with open(os.path.join(path, 'jagdschein.json'), 'w') as outfile:
+            json.dump(data_parsed, outfile)
--- train.py
+++ train.py
@@ -1,15 +1,20 @@
 import json
 import os
+import sys
 import random
+import datetime
+import pprint as pp
 
 if __name__ == '__main__':
-    # select region
-    states = os.listdir(os.path.join('data'))
-    # for n, s in enumerate(states):
-    #    print(n + 1, s)
-    # state = states[int(input('select state: ')) - 1]
+    path = os.path.join('data', 'Jagdschein MV')
+    user = input('enter your name:')
 
-    path = os.path.join('data', states[0])
+    directory = 'control_{}'.format(user)
+    if os.path.isfile(os.path.join('data', '{}.json'.format(directory))):
+        with open(os.path.join('data', '{}.json'.format(directory)), 'r') as file:
+            control = json.load(file)
+    else:
+        control = dict()
 
     with open(os.path.join(path, 'jagdschein.json'), 'r') as file:
         data = json.load(file)
@@ -33,7 +38,7 @@ if __name__ == '__main__':
             print(s)
             for q in data[subjects[n]].keys():
                 if 'info' not in q:
-                    questions.append((n + 1, q))
+                    questions.append((n, q))
     else:
         # selected subject
         for q in list(data[subjects[int(subject_id) - 1]].keys()):
@@ -45,14 +50,54 @@ if __name__ == '__main__':
     print('\nZum Beenden bitte "e" eingeben!!!')
     while True:
         random_choice = random.choice(questions)
-        # text
         answer = ''
         if data[subjects[random_choice[0]]][random_choice[1]]['type'] == 'text':
-            print('\nFrage {}:\n{}\n'.format(random_choice[1], data[subjects[random_choice[0]]][random_choice[1]]['question']))
+            print('\nFrage {}-{}:\n{}\n'.format(random_choice[0]+1, random_choice[1], data[subjects[random_choice[0]]][random_choice[1]]['question']))
+            while True:
+                answer = input('\r')
+                break
+            sys.stdout.write('CURSOR_UP_ONE')
+            print('\r\rAntwort:\n{}\n'.format(data[subjects[random_choice[0]]][random_choice[1]]['answer']))
+
             while True:
                 answer = input('Richtig(r), Falsch(f)')
                 if answer in ['r', 'f', 'e']:
                     break
+
+                if answer == 'r':
+                    if control.get(random_choice[0]):
+                        if control[random_choice[0]].get(random_choice[1]):
+                            control[random_choice[0]][random_choice[1]]['counter'] += 1
+                            control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+                        else:
+                            control[random_choice[0]] = dict()
+                            control[random_choice[0]][random_choice[1]] = dict()
+                            control[random_choice[0]][random_choice[1]]['counter'] = 0
+                            control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+
+                    else:
+                        control[random_choice[0]] = dict()
+                        control[random_choice[0]][random_choice[1]] = dict()
+                        control[random_choice[0]][random_choice[1]]['counter'] = 1
+                        control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+
+                elif answer == 'f':
+                    if control.get(random_choice[0]):
+                        if control[random_choice[0]].get(random_choice[1]):
+                            control[random_choice[0]][random_choice[1]]['counter'] = 0
+                            control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+                        else:
+                            control[random_choice[0]] = dict()
+                            control[random_choice[0]][random_choice[1]] = dict()
+                            control[random_choice[0]][random_choice[1]]['counter'] = 0
+                            control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+
+                    else:
+                        control[random_choice[0]] = dict()
+                        control[random_choice[0]][random_choice[1]] = dict()
+                        control[random_choice[0]][random_choice[1]]['counter'] = 0
+                        control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+
             if answer == 'e':
                 while True:
                     close = input('Soll das Training beendet werden (j/n)?')
@@ -60,11 +105,9 @@ if __name__ == '__main__':
                         break
                 if close == 'j':
                     break
-            else:
-                print('Antwort:\n{}\n'.format(data[subjects[random_choice[0]]][random_choice[1]]['answer']))
         # multiple
         elif data[subjects[random_choice[0]]][random_choice[1]]['type'] == 'multiple_choice':
-            print('\nFrage {}:\n{}'.format(random_choice[1], data[subjects[random_choice[0]]][random_choice[1]]['question']))
+            print('\nFrage {}-{}:\n{}\n'.format(random_choice[0]+1, random_choice[1], data[subjects[random_choice[0]]][random_choice[1]]['question']))
             for n, c in enumerate(data[subjects[random_choice[0]]][random_choice[1]]['answer']['choice']):
                 print('\t', n + 1, c)
             while True:
@@ -81,8 +124,44 @@ if __name__ == '__main__':
 
             elif int(answer) - 1 in data[subjects[random_choice[0]]][random_choice[1]]['answer']['correct']:
                 print('Richtig')
+                if control.get(random_choice[0]):
+                    if control[random_choice[0]].get(random_choice[1]):
+                        control[random_choice[0]][random_choice[1]]['counter'] += 1
+                        control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+                    else:
+                        control[random_choice[0]] = dict()
+                        control[random_choice[0]][random_choice[1]] = dict()
+                        control[random_choice[0]][random_choice[1]]['counter'] = 0
+                        control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+
+                else:
+                    control[random_choice[0]] = dict()
+                    control[random_choice[0]][random_choice[1]] = dict()
+                    control[random_choice[0]][random_choice[1]]['counter'] = 1
+                    control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+
+                print(control)
             else:
                 print('Falsch', data[subjects[random_choice[0]]][random_choice[1]]['answer']['correct'])
+
+                if control.get(random_choice[0]):
+                    if control[random_choice[0]].get(random_choice[1]):
+                        control[random_choice[0]][random_choice[1]]['counter'] = 0
+                        control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+                    else:
+                        control[random_choice[0]] = dict()
+                        control[random_choice[0]][random_choice[1]] = dict()
+                        control[random_choice[0]][random_choice[1]]['counter'] = 0
+                        control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+
+                else:
+                    control[random_choice[0]] = dict()
+                    control[random_choice[0]][random_choice[1]] = dict()
+                    control[random_choice[0]][random_choice[1]]['counter'] = 0
+                    control[random_choice[0]][random_choice[1]]['date'] = datetime.datetime.now().isoformat()
+
+    with open(os.path.join('data', '{}.json'.format(directory)), 'w') as file:
+        json.dump(control, file)
 
     print('Das Training wird beendet, Aktueller Stand wird gespeichert!')
 
